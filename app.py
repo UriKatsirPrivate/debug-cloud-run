2142eb42f63a6324e3a36f65fbe1b81daae53ab8
Uri Katsir <46541339+UriKatsirPrivate@users.noreply.github.com>
2022-08-30 21:45:28 +0300

    add global var

--- app.py
+++ app.py
@@ -4,15 +4,14 @@ A sample Hello World server.
 import os
 import rook
 
-
 from flask import Flask, render_template
 
 rook.start(token='1a43c7f087d9c43341653af043d498bc800f22a5bc6a70b1e0c0fe232b8b3b3e', labels={"env":"dev"})
 
-
 # pylint: disable=C0103
 app = Flask(__name__)
 
+region = "eu" # This is used to showcase Rookout
 
 @app.route('/')
 def hello():
