78a21719b4456dfcb8106fe77c055d55d120b556
emarco <emarco@google.com>
2022-10-30 13:39:43 +0200

    cleaned app.py

--- app.py
+++ app.py
@@ -1,31 +1,25 @@
-# from ast import If
-from cmath import log
-from distutils.log import debug
 import os
-# import rook
 from flask import Flask
 from flask import render_template, request
 from currency_converter import CurrencyConverter
-#rook.start() # Rookout token is stored in Secret Manager and is loaded into an Environment Variable (ROOKOUT_TOKEN) during deployment.
 
 app = Flask(__name__)
-# region = os.environ.get("region")
-region = "eu" # This is used to showcase Rookout
+
 
 @app.route("/")
 def form():
 
     return render_template("form.html")
 
+
 @app.route("/", methods=["POST"])
 def my_form_post():
     c = CurrencyConverter()
-
     euros = request.form["euros"]
     usd = round(c.convert(euros, "EUR", "USD"), 2)
 
-       # region = os.environ.get("region")
     return render_template("form.html", euros=euros, usd=usd)
-    
+
+
 if __name__ == "__main__":
-    app.run(debug=True,host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
